84c877a7dbc3002da59dc573355ffee7dacb1dbb
cburdick <cliff.burdick@viasat.com>
2020-01-17 16:00:20 -0500

    Added NodePort service for gRPC

--- test/RPCTest.py
+++ test/RPCTest.py
@@ -1,11 +1,11 @@
 import sys
 
-sys.path.insert(0, '../obj/')
+sys.path.insert(0, '../')
 import grpc
-import nhd_stats_pb2
-import nhd_stats_pb2_grpc
+from nhd.proto import nhd_stats_pb2
+from nhd.proto import nhd_stats_pb2_grpc
 
-SERVER_INFO = 'localhost:45655'
+SERVER_INFO = 'localhost:31044'
 
 if __name__ == "__main__":
     print('Starting RPC tests')
